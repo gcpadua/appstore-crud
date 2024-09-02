e14008e6e48a5ae0d3c686e97e08fc9ae2113622
Gustavo Pádua <gustavo.campos.padua@gmail.com>
2024-09-02 17:58:09 -0300

    Adicionadas novas tela de usuario

---
+++ src/app/usuario/lista-compra/lista-compra.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // Importa o HttpClient
+import { CommonModule } from '@angular/common';
+
+interface Aplicativo {
+  id_aplicativo: number;
+  nome: string;
+  descricao: string;
+  preco: number;
+  id_desenvolvedor: number;
+}
+
+interface ItemCarrinho {
+  app: Aplicativo;
+  quantidade: number;
+}
+
+interface responseCompra {
+  mensagem: string;
+}
+
+@Component({
+  selector: 'lista-compra',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './lista-compra.component.html',
+  styleUrl: './lista-compra.component.css'
+})
+export class ListaCompraComponent implements OnInit {
+  constructor(private http: HttpClient) {} // Injeta o HttpClient
+
+  @Input() usuarioId: number = 0;
+
+  lista_apps: Aplicativo[] = [];
+  listaCompras: ItemCarrinho[] = []; 
+
+  resposta_Compra: string = '';
+
+  finalizarCompra() {
+    const compraData = {
+      usuarioId: this.usuarioId,
+      listaCompras: this.listaCompras
+    };
+
+    console.log('Iniciando compra: ', compraData);
+    this.http.post<responseCompra>('http://localhost:3000/comprarCarrinho', compraData).subscribe({
+      next: (response) => {
+        console.log('Resposta da API:', response);
+        this.resposta_Compra = response.mensagem;
+        this.listaCompras = [];
+       },
+      error: (error) => {
+        console.error('Erro na requisição:', error);
+      }
+    });
+  }
+
+  adicionarAoCarrinho(app: Aplicativo, quantidade: number){
+    if (quantidade > 0) { // Verifica se a quantidade é válida
+      const itemExistente = this.listaCompras.find(item => item.app.id_aplicativo === app.id_aplicativo);
+      
+      if (itemExistente) {
+        itemExistente.quantidade += quantidade; // Atualiza a quantidade se o item já estiver no carrinho
+      } else {
+        this.listaCompras.push({ app, quantidade }); // Adiciona novo item ao carrinho
+      }
+      console.log('Aplicativo adicionado ao carrinho:', app, 'Quantidade:', quantidade);
+    } else {
+      console.error('Quantidade inválida.');
+    }
+  }
+
+  ngOnInit(): void {
+    this.http.get<Aplicativo[]>('http://localhost:3000/select/aplicativo').subscribe({
+      next: (response) => {
+        this.lista_apps = response;
+      },
+      error: (error) => {
+        console.error('Erro na requisição:', error);
+      }
+    });
+  }
+}
--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from "./login/login.component";
+import { ListaCompraComponent } from "./lista-compra/lista-compra.component";
+import { InfoBannerComponent } from "./info-banner/info-banner.component";
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
   selector: 'app-usuario',
   standalone: true,
-  imports: [CommonModule, LoginComponent],
+  imports: [CommonModule, LoginComponent, ListaCompraComponent, InfoBannerComponent, RegisterComponent],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
 export class UsuarioComponent {
-  usuaioLogado: number = 0;
+  usuarioLogado: number = 1;
+  login_register: number = 0;
+  comprar_historico: number = 0;
+
   handleLogin(numberReceived: number) {
-    this.usuaioLogado = numberReceived;
+    this.usuarioLogado = numberReceived;
   }
 }
